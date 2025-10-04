692124164056052abe1c1e767a216e67d8c0afcf
Krishna <ravichandrakrishna555@gmail.com>
2025-10-04 20:11:55 +0530

    test

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, UseGuards, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './users/current-user.decorator';
+import { UserDocument } from './users/models/users.schema';
+import { Response } from 'express';
 
+@Controller('auth')
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    console.log('user -> ', user);
+    await this.authService.login(user, response);
+    response.send(user);
   }
 }
